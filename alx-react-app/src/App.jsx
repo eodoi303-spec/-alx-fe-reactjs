e1173880c6588980f50cda76309cec027cb36e75
Stedan Ampofo <stedanampofo19@gmail.com>
2026-01-10 20:47:18 +0000

    add user profile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -8,6 +8,7 @@ import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
 
+import UserProfile from './components/UserProfile';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -43,6 +44,17 @@ function App() {
       <MainContent />
       <Footer />
     </div>
+
+   
+<div>
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+    </div>
+
+
     </>
   )
 }
