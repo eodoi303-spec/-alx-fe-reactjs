a4b1bf1afe96e7805e6c349171e94ca700e89d6f
Stedan Ampofo <stedanampofo19@gmail.com>
2026-01-10 19:14:07 +0000

    add header footer main content

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,6 @@
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -33,7 +36,11 @@ function App() {
     <div>
       <WelcomeMessage />
     </div>
-
+  <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
     </>
   )
 }
